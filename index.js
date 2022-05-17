8907bc156e291f192fa7f101304993b44cfd4037
Victor <vx_zz@hotmail.com>
2022-05-17 06:47:44 -0400

    commit tree 68e4b3f336e48b6f2eb19b4c58bfbfd9ef391f55

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+const num3 = 20
+const num4 = 8
+
+const multiply = num1*num2
+
+const random = Math.ceil(Math.random() * 10)
+
+const mod = num3%num4
+
+const max = Math.max(3, 5, 6, 18, 20)
